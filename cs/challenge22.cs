1f00744bc615f618edba28f7fe21f112c2fa1158
KoiKoiiKoiii <caninenote@gmail.com>
2025-04-14 14:33:24 -0400

    Create challenge22.cs

---
+++ cs/challenge22.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class DnaStrand
+{
+    public static string MakeComplement(string dna)
+    {
+        Dictionary<char, char> myDictionary = new Dictionary<char, char>();
+
+        string result = "";
+
+        myDictionary.Add('A', 'T');
+        myDictionary.Add('T', 'A');
+        myDictionary.Add('G', 'C');
+        myDictionary.Add('C', 'G');
+
+        foreach (var pair in dna)
+        {
+            result += myDictionary[pair];
+        }
+
+        return (result);
+    }
+}
